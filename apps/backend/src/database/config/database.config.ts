bfce1ec780b26bc0bfccb6c8b05a60d999411311
SofienRogue <sofien.abidi@digitalroguewave.com>
2025-12-04 19:44:04 +0100

    production build fix

--- apps/backend/src/database/config/database.config.ts
+++ apps/backend/src/database/config/database.config.ts
@@ -13,33 +13,23 @@ import { DatabaseConfig } from './database-config.type.js';
 import validateConfig from '../../utils/validate-config.js';
 
 class EnvironmentVariablesValidator {
-  @ValidateIf((envValues) => envValues.DATABASE_URL)
-  @IsString()
-  DATABASE_URL!: string;
-
-  @ValidateIf((envValues) => !envValues.DATABASE_URL)
   @IsString()
   DATABASE_TYPE!: string;
 
-  @ValidateIf((envValues) => !envValues.DATABASE_URL)
   @IsString()
   DATABASE_HOST!: string;
 
-  @ValidateIf((envValues) => !envValues.DATABASE_URL)
   @IsInt()
   @Min(0)
   @Max(65535)
   DATABASE_PORT!: number;
 
-  @ValidateIf((envValues) => !envValues.DATABASE_URL)
   @IsString()
   DATABASE_PASSWORD!: string;
 
-  @ValidateIf((envValues) => !envValues.DATABASE_URL)
   @IsString()
   DATABASE_NAME!: string;
 
-  @ValidateIf((envValues) => !envValues.DATABASE_URL)
   @IsString()
   DATABASE_USERNAME!: string;
 
